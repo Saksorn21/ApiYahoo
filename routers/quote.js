f5b1bccffa6e1e7ceebf1e49d98f0ee37d1aa3ca
Saksorn <boat_saksorn@icloud.com>
2025-08-04 09:56:25 +0000

    Don

--- routers/quote.js
+++ routers/quote.js
@@ -6,28 +6,39 @@ const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 const url = "https://finance.yahoo.com/quote/AAPL";
-function quote(req, res) {
+export default async function quote(req, res) {
    const symbol = req.params.symbol.toUpperCase();
      const cached = cache.get(symbol);
      if (cached) return res.json({ symbol, price: cached });
 
      try {
-       const { data } = await axios.get(`https://finance.yahoo.com/quote/${symbol}`, { headers });
+       const { data } = await axios.get(`https://finance.yahoo.com/quote/${symbol}`, { headers })
        const $ = cheerio.load(data);
-       const price = $('fin-streamer[data-field="regularMarketPrice"]').first().text();
+       const price = $('span[data-testid="qsp-price"]').first().text();
        cache.set(symbol, price);
        res.json({ symbol, price });
      } catch (err) {
        res.status(500).json({ error: "Fetch error" });
      }
    }
-}
+
 
 const getPrice = async () => {
   const { data } = await axios.get(url); // โหลด HTML
   const $ = cheerio.load(data);         // ยัดเข้า cheerio
-  const price = $('fin-streamer[data-field="regularMarketPrice"]').first().text();
+  const price = $('span[data-testid="qsp-price"]').first().text();
   console.log("ราคาหุ้น AAPL:", price);
 };
 
 getPrice();
+
+import fs from "fs";
+
+
+const fetchAndSaveHTML = async () => {
+  const { data: html } = await axios.get(url, { headers });
+  fs.writeFileSync("aapl.html", html);
+  console.log("HTML ถูกบันทึกแล้วเป็นไฟล์ aapl.html ✅");
+};
+
+fetchAndSaveHTML();
