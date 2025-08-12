5645ff34eec3f5799dbb357db4f3a10d3f51e455
Saksorn <boat_saksorn@icloud.com>
2025-08-12 13:55:30 +0000

    Summary quote

--- api/quote.js
+++ api/quote.js
@@ -6,8 +6,6 @@ const cache = new NodeCache({ stdTTL: 300 });
 const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
-
-
 export default async function quotes(req, res) {
    const symbol = req.params.symbol.toUpperCase();
      const cached = cache.get(symbol);
@@ -15,7 +13,10 @@ export default async function quotes(req, res) {
   //https://query1.finance.yahoo.com/v8/finance/chart/V?interval=1d&range=1d
      try {
        const { data } = await axios.get(`/v8/finance/chart/${symbol}?interval=1d&range=1d`)
+       const summary = {}
        const meta = data.chart.result[0].meta
+      meta.regularMarketChange = meta.regularMarketPrice - meta.chartPreviousClose;
+       meta.regularMarketChangePercent = (regularMarketChange / meta.chartPreviousClose) * 100;
        delete meta.currentTradingPeriod
        delete meta.validRanges
        delete meta.dataGranularity
@@ -27,86 +28,159 @@ export default async function quotes(req, res) {
        res.status(500).json({ error: "Fetch error" });
      }
    }
+function getMarketStatus(currentTradingPeriod) {
+  const now = Math.floor(Date.now() / 1000);
 
+  const pre = currentTradingPeriod.pre;
+  const regular = currentTradingPeriod.regular;
+  const post = currentTradingPeriod.post;
 
-const getPrice = async (ticker) => {
-  const symbol = ticker.toUpperCase().trim()
-  const url = `https://finance.yahoo.com/quote/${symbol}`;
-  console.log(symbol, url)
-  try {
-     
-  
-  const { data } = await axios.get(url, { headers }); // โหลด HTML
-  const $ = cheerio.load(data);         // ยัดเข้า cheerio
-  
-  const quote = {}
-    const exchangeText = $('span.exchange').first().text().trim();
-    const marketName = exchangeText.split(" - ")[0];  // เอาก่อน '-'
+  if (now >= pre.start && now <= pre.end) return "PRE";
+  if (now >= regular.start && now <= regular.end) return "REGULAR";
+  if (now >= post.start && now <= post.end) return "POST";
+  return "CLOSED"; // อยู่นอกช่วงตลาดเปิด
+}
 
-    // ดึงชื่อบริษัทและ ticker เช่น "NVIDIA Corporation (NVDA)"
-    const titleText = $('title').text().trim();
-    const match = titleText.match(/^(.*?) \(([^)]+)\)/);
-    let companyName = "";
+function parsePrePostMacket(price, summary,status) {
+     switch (status) {
+       case "PRE":
+         summary.regularMarketPrice = price.regPrice;
+         summary.regularMarketChange = price.regChange;
+         summary.regularMarketChangePercent = price.regChangePct;
+         
+         summary.preMarketPrice = price.prePrice;
+         summary.preMarketPriceChange = price.preChange;
+         summary.preMarketChangePercent = price.preChangePct;
+         break;
+       case "REGULAR":
+         summary.regularMarketPrice = price.regPrice;
+           summary.regularMarketChange = price.regChange;
+           summary.regularMarketChangePercent = price.regChangePct;
+         //pre
+         summary.preMarketPrice = price.prePrice;
+          summary.preMarketPriceChange = price.preChange;
+          summary.preMarketChangePercent = price.preChangePct;
+         break;
+       case "POST":
+         summary.regularMarketPrice = price.regPrice;
+         summary.regularMarketChange = price.regChange;
+         summary.regularMarketChangePercent = price.regChangePct;
+           summary.postMarketPrice = price.postPrice
+        summary.postMarketChange = price.postChange
+           summary.postMarketChangePercent = price.postChangePct;
+         break;
+       default:
+         summary.regularMarketPrice = price.regPrice;
+          summary.regularMarketChange = price.regChange;
+          summary.regularMarketChangePercent = price.regChangePct;
 
-    if (match) {
-      companyName = match[1];  // NVIDIA Corporation
-    }     
-  const price = $('span[data-testid="qsp-price"]').first().text();
-  const change = $('span[data-testid="qsp-price-change"]').first().text()
-  const cache = new NodeCache({stdTTL: 300})
-  const cached = cache.get(ticker)
-  
-  const open =  $('fin-streamer[data-field="regularMarketOpen"]').first().text()
-  const changePercent = $('span[data-testid="qsp-price-change-percent"]').first().text().replace(/[(),%]/g, "")
-  const prePrice =  $('span[data-testid="qsp-pre-price"]').first().text()
-  const preChange = $('span[data-testid="qsp-pre-price-change"]').first().text()
-  const preChangePercent = $('span[data-testid="qsp-pre-price-change-percent"]').first().text().replace(/[(),%]/g, "")
-  const postPrice =  $('span[data-testid="qsp-post-price"]').first().text()
-  const postChange = $('span[data-testid="qsp-post-price-change"]').first().text()
-  const postChangePercent = $('span[data-testid="qsp-post-price-change-percent"]').first().text().replace(/[(),%]/g, "")
-  const market = $('fin-streamer[changeev="timeChange"]').attr("data-field")
-  quote.symbol = symbol
-  quote.longName = companyName
-  quote.exchange = marketName
-  quote.price = price.trim()
-  quote.change = change.trim()
-  quote.changePercent = changePercent.trim()
-  if(market.trim() === 'preMarketTime'){
-    quote.prePrice = prePrice.trim()
-    quote.preChange = preChange.trim()
-    quote.preChangePercent = preChangePercent.trim()
-  }else{
-    quote.postPrice = postPrice.trim()
-    quote.postChange = postChange.trim()
-    quote.postChangePercent = postChangePercent.trim()
+         summary.preMarketPrice = meta.prePrice;
+          summary.preMarketPrice = price.preChange;
+          summary.preMarketChangePercent = price.preChangePct;
+         summary.postMarketPrice = price.postPrice
+         summary.postMarketChange = price.postChange
+            summary.postMarketChangePercent = price.postChangePct;
+     }
+   }
+function filterTodayData(timestamps, closes) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const todayStart = Math.floor(today.getTime() / 1000);
+  const todayEnd = todayStart + 86400;
+
+  return timestamps
+    .map((t, i) => ({ time: t, price: closes[i] }))
+    .filter(x => x.time >= todayStart && x.time < todayEnd && x.price != null);
+}
+
+function getLastPriceInPeriod(timestamps, closes, period) {
+  const OFFSET = 60; // ขยับให้เผื่อ 1 นาที
+  const start = period.start - OFFSET;
+  const end = period.end + OFFSET;
+
+  const filtered = timestamps
+    .map((t, i) => ({ time: t, price: closes[i] }))
+    .filter(x => x.time >= start && x.time <= end && x.price != null);
+
+  if (!filtered.length) return null;
+  return filtered[filtered.length - 1].price;
+}
+function buildPriceSummary(timestamps, closes, meta) {
+  const { pre, regular, post } = meta.currentTradingPeriod;
+
+  const prePrice = getLastPriceInPeriod(timestamps, closes, pre);
+  const regPrice = getLastPriceInPeriod(timestamps, closes, regular);
+  const postPrice = getLastPriceInPeriod(timestamps, closes, post);
+
+  return {
+    regPrice,
+    regChange: regPrice && meta.chartPreviousClose ? regPrice - meta.chartPreviousClose : null,
+    regChangePct: regPrice && meta.chartPreviousClose ? ((regPrice - meta.chartPreviousClose) / meta.chartPreviousClose) * 100 : null,
+    prePrice,
+    preChange: prePrice && meta.chartPreviousClose ? prePrice - meta.chartPreviousClose : null,
+    preChangePct: prePrice && meta.chartPreviousClose ? ((prePrice - meta.chartPreviousClose) / meta.chartPreviousClose) * 100 : null,
+    postPrice,
+    postChange: postPrice && regPrice ? postPrice - regPrice : null,
+    postChangePct: postPrice && regPrice ? ((postPrice - regPrice) / regPrice) * 100 : null
   }
-    
-  const stats = {};
+}
+function parseChartData(chartData, summary) {
+  const meta = chartData.chart.result[0].meta;
+  const timestamps = chartData.chart.result[0].timestamp
+  
+  const indicators = chartData.chart.result[0].indicators.quote[0]; // quote เป็น array เดียว
+  const closes = indicators.close;
+  const periods = meta.currentTradingPeriod;
+  const prevClose = meta.chartPreviousClose
+  const lastIndex = timestamps.length - 1;
+  const prevIndex = lastIndex - 1;
+  const todayData = filterTodayData(timestamps, closes);
+  const marketStatus = getMarketStatus(meta.currentTradingPeriod);
+  summary.marketStatus = marketStatus
+  const regularMarketPrice = indicators.close[lastIndex];
+  const regularMarketPreviousClose = indicators.close[prevIndex];
 
-  $("fin-streamer").each((_, el) => {
-    const field = $(el).attr("data-field");
-    const value = $(el).text().trim();
-    if (field && value) {
-        quote[field] = value;
-    }
-  });
+  const prices = buildPriceSummary(timestamps, closes, meta);
+  // สถานะตลาดปัจจุบัน
+  // marketState อาจไม่มีใน meta ต้องสร้าง logic กำหนดเอง
+  
+  parsePrePostMacket(prices, summary, marketStatus)
+  return {
+    symbol: meta.symbol,
+    longName: meta.longName,
+    shortName: meta.shortName,
+    exchangeName: meta.exchangeName,
+    fullExchangeName: meta.fullExchangeName,
+    instrumentType: meta.instrumentType,
+    firstTradeDate: meta.firstTradeDate,
+    regularMarketTime: meta.regularMarketTime,
+    marketStatus,
+    hasPrePostMarketData: meta.hasPrePostMarketData,
+    gmtoffset: meta.gmtoffset,
+    timezone: meta.timezone,
+    exchangeTimezoneName: meta.exchangeTimezoneName,
+    regularMarketPrice,
+    currency: meta.currency,
+    chartPreviousClose: meta.chartPreviousClose,
+    priceHint: meta.priceHint
+  };
+}
 
-  console.log(stats)
-  console.log("ราคาหุ้น AAPL:", price, open);
+
+async function testq() {
+   try {
+      const { data } = await axios.get(`/v8/finance/chart/NVDA?interval=1m&range=2d`)
+       const summary = {}
+     const meta = data.chart.result[0].meta
+     const result = {...parseChartData(data,summary),...summary}
+     delete meta.currentTradingPeriod
+      delete meta.validRanges
+      delete meta.dataGranularity
+      delete meta.range
+     console.log(result);
+   } catch (err) {
+      console.error(err)
+   }
    
-  quote.marksetState = market
-  console.log(quote)
-    } catch (error) {
-console.error(error)
-    }
-};
-const url = "https://query1.finance.yahoo.com/v8/finance/chart/NVDA?range=1d&interval=2m";
-/** 
-axios.get(url).then(res => {
-  const result = res.data.chart.result[0];
-  const lastClose = result.meta.regularMarketPrice;
-  console.log()
-  console.log("ราคาล่าสุด BTC:", lastClose);
-});
-//getPrice("NVDA");
-*/
+}
+(async () => await testq())()
